2e15845dc89ccb7316c53f190b515fd56e8284e9
Tim Beishuizen - BMT lab <timbeishuizen@hotmail.com>
2018-04-23 13:55:14 +0200

    Continuation feature reduction

--- Testing/FeatureReduction/EvaluationWrapperMethods.py
+++ Testing/FeatureReduction/EvaluationWrapperMethods.py
@@ -2,11 +2,13 @@ from FeatureReductionMethods import WrapperMethods as WM
 from DataExtraction import DataSetExtraction as DSE
 from sklearn import model_selection as SMS
 from sklearn import linear_model as SLM
+from sklearn import metrics as SME
 import numpy as np
 import csv
+import time
 
-used_data = ["MicroOrganisms"]
-algorithms = ["forward", "backward"]
+used_data = ["Psoriasis"]
+algorithms = ["floating"]
 orders = ["Random", "MI"]
 thresholds = [0.01, 0.001]
 
@@ -17,12 +19,19 @@ threshold_name = []
 T_val = []
 T_test = []
 T_feat = []
+T_time = []
+precision = []
+recall = []
+F1 = []
+
+index = 1
 
 for name in used_data:
     for test in algorithms:
         for order in orders:
             for threshold in thresholds:
-                print("Currently at data %s, test %s, ordering %s, and threshold %f" % (name, test, order, threshold))
+                print("Currently at test %i of 12, data %s, test %s, ordering %s, and threshold %f" % (index, name, test, order, threshold))
+                index += 1
 
                 data_name.append(name)
                 threshold_name.append(threshold)
@@ -35,13 +44,27 @@ for name in used_data:
                 # Split data in training and test data.
                 X_train, X_test, y_train, y_test = SMS.train_test_split(X, y)
 
+                start_time = time.clock()
                 if test == 'forward':
                     X_new, feat_new, _ = WM.forward_selection(X_train, y_train, features, ranking_method=order,
-                                                           scoring_method='nb', improvement_threshold=threshold, scoring_cv=8)
+                                                           scoring_method='nb', improvement_threshold=threshold, scoring_cv=5)
                 elif test == 'backward':
                     X_new, feat_new = WM.backward_selection(X_train, y_train, features, ranking_method=order,
-                                                           scoring_method='nb', improvement_threshold=threshold, scoring_cv=8)
+                                                           scoring_method='nb', improvement_threshold=threshold, scoring_cv=5)
+                elif type(test) == list:
+                    X_new, feat_new = WM.sequential_search(X_train, y_train, features, sel_seq='FB', n_sel_seq=test,
+                                                           scoring_method='nb', scoring_cv=5,
+                                                           improvement_threshold=threshold,
+                                                           ranking_method=order, continued_search=True)
 
+                elif test == 'floating':
+                    X_new, feat_new = WM.sequential_search(X_train, y_train, features, sel_seq='FB', n_sel_seq=[1300 - 1, 1300 - 1],
+                                         scoring_method='nb', scoring_cv=5, improvement_threshold=threshold,
+                                         ranking_method=order, continued_search=False)
+
+                elapsed = time.clock() - start_time
+                T_time.append(elapsed)
+                print("Time elapsed: %f" % elapsed)
                 print("number of features is %i" % len(feat_new))
                 T_feat.append(len(feat_new))
 
@@ -71,10 +94,16 @@ for name in used_data:
                 ml = SLM.LogisticRegression()
                 ml.fit(X_new, y_train)
                 T_test.append(ml.score(X_test_new, y_test))
+                y_pred = ml.predict(X_test_new)
+                prec, rec, Fbeta, _ = SME.precision_recall_fscore_support(y_test, y_pred)
+                precision.append(sum(prec)/len(prec))
+                recall.append(sum(rec)/len(rec))
+                F1.append(sum(Fbeta)/len(Fbeta))
+
 
                 print("Validation score is %f, test score is %f" % (T_val[-1], T_test[-1]))
 
-with open('Wrapper_Forward_Backward_values.csv', 'w', newline='') as csv_file:
+with open('Psoriasis_Wrapper_Float_values.csv', 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(data_name)
     csv_writer.writerow(test_name)
@@ -83,3 +112,7 @@ with open('Wrapper_Forward_Backward_values.csv', 'w', newline='') as csv_file:
     csv_writer.writerow(T_val)
     csv_writer.writerow(T_test)
     csv_writer.writerow(T_feat)
+    csv_writer.writerow(T_time)
+    csv_writer.writerow(precision)
+    csv_writer.writerow(recall)
+    csv_writer.writerow(F1)
--- Testing/FeatureReductionData/VisualizeFeatureReductionEvaluation.py
+++ Testing/FeatureReductionData/VisualizeFeatureReductionEvaluation.py
@@ -4,53 +4,39 @@ import matplotlib.pyplot as PLT
 
 data_name = 'Psoriasis'
 
-values = []
+filter_values = []
 
-with open('Filter_T_values.csv', 'r', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        values.append(row)
+# with open('Filter_T_values.csv', 'r', newline='') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     for row in csv_reader:
+#         filter_values.append(row)
+#
+# filter_values = np.asarray(filter_values)
 
-extra_MI_values = []
+wrapper_values = []
 
-with open('Filter_MI_values.csv', 'r', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        extra_MI_values.append(row)
+if data_name in ['MO', 'Arcene']:
+    with open(data_name + '_Wrapper_values.csv', 'r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            wrapper_values.append(row)
 
-forward_backward_values = []
+    wrapper_values = np.asarray(wrapper_values)
 
-with open('Wrapper_Forward_Backward_values.csv', 'r', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        forward_backward_values.append(row)
+elif data_name in ['Psoriasis', 'RSCTC']:
+    quick_values = []
+    with open(data_name + '_Wrapper_Quick_values.csv', 'r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            quick_values.append(row)
 
-PTA_values = []
+    float_values = []
+    with open(data_name + '_Wrapper_Float_values.csv', 'r', newline='') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            float_values.append(row)
 
-with open('PTA_values.csv', 'r', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        PTA_values.append(row)
-
-Floating_values = []
-
-with open('Floating_values.csv', 'r', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        Floating_values.append(row)
-
-values = np.asarray(values)
-extra_MI_values = np.asarray(extra_MI_values)
-forward_backward_values = np.asarray(forward_backward_values)
-PTA_values = np.asarray(PTA_values)
-Floating_values = np.asarray(Floating_values)
-
-print(forward_backward_values.shape)
-print(PTA_values.shape)
-print(Floating_values.shape)
-
-filter_values = np.append(values, extra_MI_values, axis = 1)
-wrapper_values = np.concatenate((forward_backward_values, PTA_values, Floating_values), axis=1)
+    wrapper_values = np.concatenate((quick_values, float_values), axis=1)
 
 plot_legend = []
 
@@ -82,6 +68,7 @@ def compute_fraction(data_name, number):
     else:
         raise ValueError("Unknown data label")
 
+
 # # Plotting filter values
 # for i in range(filter_values.shape[1]):
 #     if filter_values[0, i] == 'MicroOrganisms':
@@ -94,13 +81,13 @@ def compute_fraction(data_name, number):
 
 # Plotting Wrapper values
 for i in range(wrapper_values.shape[1]):
-    if wrapper_values[0, i] == 'MicroOrganisms':
         if wrapper_values[1, i] == 'forward':
             style_label = 'b' + return_label(wrapper_values[3, i], wrapper_values[2, i])
             legend_label = wrapper_values[1, i] + ': order = ' + wrapper_values[3, i] + ', ' + \
                            wrapper_values[2, i]
 
         elif wrapper_values[1, i] == 'backward':
+            continue
             style_label = 'y' + return_label(wrapper_values[3, i], wrapper_values[2, i])
             legend_label = wrapper_values[1, i] + ': order = ' + wrapper_values[3, i] + ', ' + \
                            wrapper_values[2, i]
@@ -112,22 +99,22 @@ for i in range(wrapper_values.shape[1]):
             style_label = 'c' + return_label(wrapper_values[3, i], wrapper_values[2, i])
             legend_label = 'PTA, [l, r] = ' + wrapper_values[1, i] + ': order = ' + wrapper_values[3, i] + ', ' + \
                            wrapper_values[2, i]
-        elif wrapper_values[1, i] == '1':
+        elif wrapper_values[1, i] == 'floating':
             style_label = 'm' + return_label(wrapper_values[3, i], wrapper_values[2, i])
             legend_label = 'Floating search: order = ' + wrapper_values[3, i] + ', ' + \
                            wrapper_values[2, i]
         else:
             raise ValueError
 
-        val_score = float(wrapper_values[5, i])
+        val_score = float(wrapper_values[10, i])
         fraction = compute_fraction(wrapper_values[0, i], int(wrapper_values[6, i]))
         PLT.plot(fraction, val_score, style_label, label=legend_label)
 
-#PLT.axis([0, 1, 0, 1])
-PLT.axis([0, 0.2, 0.4, 1])
+# PLT.axis([0, 1, 0, 1])
+# PLT.axis([0, 0.2, 0.4, 1])
 PLT.xlabel("The feature fraction")
-PLT.ylabel("The test score")
-PLT.title("The spectrum for all sequential wrapper methods")
+PLT.ylabel("The F1 score")
+PLT.title("The spectrum for all sequential wrapper methods for the %s dataset" % (data_name))
 PLT.legend(loc='center left', bbox_to_anchor=(0.7, 0.3))
 PLT.show()
 
