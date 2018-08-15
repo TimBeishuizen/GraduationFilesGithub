ad9329c7169b05c72dd1b83433b0a2d25cdda314
Tim Beishuizen - BMT lab <timbeishuizen@hotmail.com>
2018-08-15 12:45:28 +0200

    Missing Value etc presentation

--- Testing/MissingValueHandlingData/PlotDistributionResults.py
+++ Testing/MissingValueHandlingData/PlotDistributionResults.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 data_names = ['HeartAttack', 'Hepatitis', 'Cirrhosis', 'Cervical']
 #data_names = ['Cirrhosis']
@@ -42,12 +43,11 @@ for i in reversed(range(len(features))):
         mean_p = np.delete(mean_p, i, axis=0)
         var_p = np.delete(var_p, i, axis=0)
 
-All_names = ['1. CCA', '2. WCA', '3. Mean', '4. Hot deck', '5. kNN', '6. Regression', '7. MICE']
+All_names = ['CCA', 'WCA', 'Mean', 'Hot deck', 'kNN, k=3', 'Regression', 'MICE']
 
 order = np.argsort(missing_percentage)[:-5]
 
 mp = np.asarray(missing_percentage)[order]
-
 
 x = np.arange(0.0, 0.15, step=0.01)
 fit_degree = 1
@@ -70,26 +70,46 @@ def poly_fit(x, args):
 
     return y
 
+fig, axes = plt.subplots(3, 3, sharex='all', sharey='all')
+
+fig.text(0.5, 0.04, 'Missing values (%)', ha='center', va='center')
+fig.text(0.06, 0.5, 'p-value mean distribution', ha='center', va='center', rotation='vertical')
+fig.suptitle('Probability of the mean originating from both the old and new distribution,'
+                                             '\nboth data points and linear regression line')
+
+
 for j in range(7):
     fit = np.polyfit(mp, mean_p[order, j], fit_degree)
-    plt.scatter(mp * 100, mean_p[order, j])
+    axes[math.floor(j / 3), j % 3].scatter(mp * 100, mean_p[order, j])
     fit_output = poly_fit(x, fit)
-    plt.plot(x * 100, fit_output)
+    axes[math.floor(j/3), j%3].plot(x * 100, fit_output)
 
-    plt.title('Probability of the mean originating from both the old and new distribution')
-    plt.xlabel('Missing values (%)')
-    plt.ylabel('p-value mean distribution')
-    plt.legend(All_names[j], loc='center left', bbox_to_anchor=(0.95, 0.5))
-    plt.show()
+    axes[math.floor(j / 3), j % 3].set_title('%s' % All_names[j])
+    #axes[math.floor(j / 3), j % 3].set_xlabel('Missing values (%)')
+    #axes[math.floor(j / 3), j % 3].set_ylabel('p-value mean distribution')
+    #plt.legend(['regression line', 'data points'], loc='center left', bbox_to_anchor=(0.95, 0.5))
+
+fig.show()
+plt.show()
+
+fig, axes = plt.subplots(3, 3, sharex='all', sharey='all')
+
+fig.text(0.5, 0.04, 'Missing values (%)', ha='center', va='center')
+fig.text(0.06, 0.5, 'p-value variance distribution', ha='center', va='center', rotation='vertical')
+fig.suptitle('Probability of the variance originating from both the old and new distribution,'
+                                             '\nboth data points and linear regression line')
+
 
 for j in range(7):
-    fit2 = np.polyfit(mp, var_p[order, j], fit_degree)
-    plt.scatter(mp * 100, var_p[order, j])
-    fit_output2 = poly_fit(x, fit2)
-    plt.plot(x * 100, fit_output2)
+    fit = np.polyfit(mp, var_p[order, j], fit_degree)
+    axes[math.floor(j / 3), j % 3].scatter(mp * 100, var_p[order, j])
+    fit_output = poly_fit(x, fit)
+    axes[math.floor(j/3), j%3].plot(x * 100, fit_output)
 
-    plt.title('Probability of the variance originating from both the old and new distribution')
-    plt.xlabel('Missing values (%)')
-    plt.ylabel('p-value variance distribution')
-    plt.legend(All_names[j], loc='center left', bbox_to_anchor=(0.95, 0.5))
-    plt.show()
+    axes[math.floor(j / 3), j % 3].set_title('%s' % All_names[j])
+    #axes[math.floor(j / 3), j % 3].set_xlabel('Missing values (%)')
+    #axes[math.floor(j / 3), j % 3].set_ylabel('p-value mean distribution')
+    #plt.legend(['regression line', 'data points'], loc='center left', bbox_to_anchor=(0.95, 0.5))
+
+fig.show()
+plt.show()
